dc6e9657fcd7116a254443151a96be69edd3ed25
Thembisile  Makamu <makamuthe2@gmail.com>
2024-10-24 14:19:22 +0200

    wimbo

--- wimbo/src/components/sidebar/index.js
+++ wimbo/src/components/sidebar/index.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import SidebarButton from './sidebarButton';
+import './sidebar.css';
+import { FaHome, FaMusic, FaList, FaCog } from 'react-icons/fa';
 
-export default function  () {
+export default function Sidebar() {
   return (
-    <div> </div>
-  )
+    <div className="sidebar-container">
+      <img
+        src=".\public\pexels-photo-1704488.webp"
+        className="pp"
+        alt="profile picture"
+      ></img>
+      <div>
+        <SidebarButton title="Home" to="" icon={<FaHome />} />
+        <SidebarButton title="Covers" to="" icon={<FaMusic />} />
+        <SidebarButton title="Playlist" to="" icon={<FaList />} />
+        <SidebarButton title="Settings" to="" icon={<FaCog />} />
+      </div>
+      <SidebarButton />
+    </div>
+  );
 }
--- wimbo/src/screens/Home/Index.js
+++ wimbo/src/screens/Home/Index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './home.css';
+import Sidebar from "../../components/sidebar";
 
 export default function Home() {
     return (
         <Router>
             <div className="main-body">
+              <Sidebar/>
                 <Routes>
-                    
+                    <Route></Route>
                 </Routes>
             </div>
         </Router>
